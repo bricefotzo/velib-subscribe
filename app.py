c8cd57cfd87befd261c4f81765a161c675054801
bricefotzo <brice.fotzo@hotmail.com>
2023-10-12 21:45:27 +0200

    feat(app): Add main application file
    Added app.py as the entry point for the application.

---
+++ app.py
@@ -0,0 +1,34 @@
+import os
+from dotenv import load_dotenv
+import streamlit as st
+from utils.ui import render_ui
+from utils.helpers import trigger_github_action
+from db.mongo import add_user, add_or_update_preference, get_user_by_email
+
+
+# Configuration
+CITIES = ["Paris", "Aubervilliers", "Nanterre"]
+
+
+def main():
+    city_id, station_name, time_slot, email, subscribe_button = render_ui(CITIES)
+
+    if subscribe_button:
+        try:
+            existing_user = get_user_by_email(email)
+            if not existing_user:
+                add_user(email, is_premium=False)
+            add_or_update_preference(email, station_name, time_slot.strftime('%H:%M'))
+
+            if trigger_github_action(email, station_name, time_slot):
+                st.success(
+                    f"Notification programmée pour {time_slot} à la station {station_name} pour l'adresse {email}!")
+            else:
+                st.error("Erreur lors du déclenchement de l'action GitHub.")
+
+        except Exception as e:
+            st.error(f"An error occurred: {e}")
+
+
+if __name__ == "__main__":
+    main()
